962fad7de701d61da3e8e18601261a77ed732b49
EinatKaZ <einatkazari@gmail.com>
2022-12-13 15:09:05 +0200

    Strength Scale Password

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect , useState} from 'react' ;
 
 function App() {
+  const [password , setPassword] = useState("") ; 
+  const [strength , setStrength] = useState("week") ; 
+  const [submit , setSubmit] = useState (false) ;
+
+  useEffect(()=>{
+    if(password.length < 3){
+      setStrength("weak")
+      setSubmit(false)
+    }
+
+    if(password.length > 2 && password.length < 7 ){
+      setStrength("medium")
+      setSubmit(true)
+    }
+
+    if(password.length >= 7 ){
+      setStrength("Strong")
+      setSubmit(true)
+    }
+
+  }, [strength,password,submit])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h1> Enter Your Password </h1>
+      <input type = "text" onChange={(e)=>{setPassword(e.target.value)}}></input>
+      <button onClick={strength} disabled = {submit ? false : true}  > Submit </button>
+      <p> Your password strength is <b>{strength}</b> </p>
+     
     </div>
   );
 }
